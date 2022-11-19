f941b763ccf37fb2d90e217bb4abc961bd7df0f0
Tsukay <djlogomolo@gmail.com>
2022-11-19 13:56:33 +0100

    Save

---
+++ src/fr/utt/LO02/VL/CestDuBrutal/Faction.java
@@ -0,0 +1,5 @@
+package fr.utt.LO02.VL.CestDuBrutal;
+
+public enum Faction {
+	ISI, MTE, A2I, RT, GM, MM, GI,
+}
--- src/fr/utt/LO02/VL/CestDuBrutal/Joueur.java
+++ src/fr/utt/LO02/VL/CestDuBrutal/Joueur.java
@@ -9,9 +9,7 @@ public class Joueur {
 	private String nomJoueur;
 	private Map<Integer, Etudiant> etudiantsDispo = new LinkedHashMap<>();
 
-	private enum faction {
-		ISI, MTE, A2I, RT, GM, MM, GI,
-	};
+	
 
 	/**
 	 * constructeur de la classe Joueur
@@ -19,22 +17,23 @@ public class Joueur {
 	 * @param nomJoueur
 	 */
 	public Joueur(String nomJoueur) {
+		System.out.println("")
 		this.nomJoueur = nomJoueur;
 		this.nbPoints = 15;
-		
-		
-		//Joueur this = new Joueur("this");
+
+		// Joueur this = new Joueur("this");
 		boolean next = true;
 		Scanner entree = new Scanner(System.in);
-		for (int i = 0; i < 5; ++i) {
-			System.out.println("Etudiant : " + i);
+		for (int i = 0; i < 5; i++) {
 			next = true;
 			this.etudiantsDispo.put(i, new Etudiant(false, 10, 10, 10, 10, 10));
 			while (this.getNbPoints() > 0 && next == true) {
+				System.out.println("Etudiant : " + i);
 				System.out.println(
-						"Choisissez un paramêtre à modifier :\n1-dexterite\n2-force\n3-resistance\n4-initiative\n5-constitution\n6-etudiant suivant\nvous avez "
+						"Choisissez un paramêtre à modifier :\n1-dexterite\n2-force\n3-resistance\n4-initiative\n5-constitution\n6-etudiant suivant\n7-etudiant précédent\nvous avez "
 								+ this.getNbPoints() + " crédits");
 				int competence = entree.nextInt();
+
 				int points_a_ajouter;
 				switch (competence) {
 				case 1:
@@ -66,10 +65,63 @@ public class Joueur {
 						System.out.println("Pas assez de crédits !");
 					}
 					break;
-				case 6:
+				case 3:
+					System.out.println("votre etudiant possède " + this.etudiantsDispo.get(i).getResistance()
+							+ " de résistance, entrez la valeur à lui ajouter :");
+					points_a_ajouter = entree.nextInt();
+					if (this.getNbPoints() - points_a_ajouter >= 0) {
+						this.setNbPoints(this.getNbPoints() - points_a_ajouter);
+						this.etudiantsDispo.get(i)
+								.setResistance(this.etudiantsDispo.get(i).getResistance() + points_a_ajouter);
+
+						System.out.println("L'etudiant numéro " + i + " a maintenant "
+								+ this.etudiantsDispo.get(i).getResistance() + " de résistance");
+					} else {
+						System.out.println("Pas assez de crédits !");
+					}
+					break;
+				case 4:
+					System.out.println("votre etudiant possède " + this.etudiantsDispo.get(i).getInitiative()
+							+ " d'initiative, entrez la valeur à lui ajouter :");
+					points_a_ajouter = entree.nextInt();
+					if (this.getNbPoints() - points_a_ajouter >= 0) {
+						this.setNbPoints(this.getNbPoints() - points_a_ajouter);
+						this.etudiantsDispo.get(i)
+								.setInitiative(this.etudiantsDispo.get(i).getInitiative() + points_a_ajouter);
+
+						System.out.println("L'etudiant numéro " + i + " a maintenant "
+								+ this.etudiantsDispo.get(i).getInitiative() + " d'initiative");
+					} else {
+						System.out.println("Pas assez de crédits !");
+					}
+					break;
+				case 5:
+					System.out.println("votre etudiant possède " + this.etudiantsDispo.get(i).getConstitution()
+							+ " de constitution, entrez la valeur à lui ajouter :");
+					points_a_ajouter = entree.nextInt();
+					if (this.getNbPoints() - points_a_ajouter >= 0) {
+						this.setNbPoints(this.getNbPoints() - points_a_ajouter);
+						this.etudiantsDispo.get(i)
+								.setConstitution(this.etudiantsDispo.get(i).getConstitution() + points_a_ajouter);
+
+						System.out.println("L'etudiant numéro " + i + " a maintenant "
+								+ this.etudiantsDispo.get(i).getConstitution() + " de constitution");
+					} else {
+						System.out.println("Pas assez de crédits !");
+					}
+					break;
+				case 6:// suivant
 					next = false;
 					break;
-					
+				case 7:// précédent
+					next = false;
+					if (i - 2 >= 0) {
+						i -= 2;
+					}
+					break;
+				default:
+					System.out.println("veuillez entrer une des valeurs propose");
+					break;
 				}
 
 			}
@@ -129,11 +181,11 @@ public class Joueur {
 	}
 
 	public void definirStrategie(Etudiant strategieEtu, Strategie typeStrategie) {
-
+		
 	}
 
 	public void choixReserviste(Etudiant EtuReserviste, boolean EstReserviste) {
-
+		EtuReserviste.setReserviste(EstReserviste);
 	}
 
 	public static void main(String[] args) {
