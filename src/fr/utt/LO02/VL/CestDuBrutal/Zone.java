c25712d0f3354320be086d5622b900e423d45e97
Tsukay <djlogomolo@gmail.com>
2022-11-22 15:50:17 +0100

    Save

--- src/fr/utt/LO02/VL/CestDuBrutal/Zone.java
+++ src/fr/utt/LO02/VL/CestDuBrutal/Zone.java
@@ -1,28 +1,50 @@
 package fr.utt.LO02.VL.CestDuBrutal;
+
 import java.util.Set;
 import java.util.HashSet;
 
 public class Zone {
-	
-	Set<Etudiant> set =  new HashSet<Etudiant>();
+
+	Faction factionJ1;
+	Faction factionJ2;
+	Set<Etudiant> etuJoueur1 = new HashSet<Etudiant>();
+	Set<Etudiant> etuJoueur2 = new HashSet<Etudiant>();
 	private Joueur estControleePar;
 	private String nomZone;
-	private int nombreEtu; //penser à modifier le lien Zone/Etudiant dans le diagramme de classes !!
+	private int nombreEtu; // penser à modifier le lien Zone/Etudiant dans le diagramme de classes !!
 
 	/**
 	 * constructeur de la classe Zone
 	 */
-	public Zone() {
-		//A compléter
+	public Zone(String nomZone, Faction factionJ1, Faction factionJ2) {
+		// A compléter
+		this.nomZone = nomZone;
+		this.factionJ1 = factionJ1;
+		this.factionJ2 = factionJ2;
+
 	}
+
 	/**
 	 * @return the estControleePar
 	 */
+	public void affecterEtudiant(Etudiant etudiant) {
+		if (etudiant.getFactionEtu() == factionJ1) {
+			etuJoueur1.add(etudiant);
+		} else if (etudiant.getFactionEtu() == factionJ2) {
+			etuJoueur2.add(etudiant);
+		}
+	}
+	public void desaffecterEtudiant(Etudiant etudiant) {
+		if (etudiant.getFactionEtu() == factionJ1) {
+			etuJoueur1.remove(etudiant);
+		} else if (etudiant.getFactionEtu() == factionJ2) {
+			etuJoueur2.remove(etudiant);
+		}
+	}
+
 	public Joueur getEstControleePar() {
 		return estControleePar;
 	}
-
-
 
 	/**
 	 * @param estControleePar the estControleePar to set
@@ -31,16 +53,12 @@ public class Zone {
 		this.estControleePar = estControleePar;
 	}
 
-
-
 	/**
 	 * @return the nomZone
 	 */
 	public String getNomZone() {
 		return nomZone;
 	}
-
-
 
 	/**
 	 * @param nomZone the nomZone to set
@@ -49,8 +67,6 @@ public class Zone {
 		this.nomZone = nomZone;
 	}
 
-
-
 	/**
 	 * @return the nombreEtu
 	 */
@@ -58,16 +74,12 @@ public class Zone {
 		return nombreEtu;
 	}
 
-
-
 	/**
 	 * @param nombreEtu the nombreEtu to set
 	 */
 	public void setNombreEtu(int nombreEtu) {
 		this.nombreEtu = nombreEtu;
 	}
-
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
