c08e688a3ea332afd6943d7d3f423898d703c01e
Vincent Delestre <vdelestre02@gmail.com>
2022-11-29 14:27:00 +0100

    choix PV&Initiative puis action

--- src/fr/utt/LO02/VL/CestDuBrutal/Zone.java
+++ src/fr/utt/LO02/VL/CestDuBrutal/Zone.java
@@ -29,21 +29,34 @@ public class Zone {
 		this.getNomZone();
 		int bestInitiativeJ1 = 0;
 		int bestInitiativeJ2 = 0;
+		int moinsCreditsJ1 = 100;
+		int moinsCreditsJ2 = 100;
+		// certaines variables Etudiant sont initialisées comme nulles car elles sont
+		// modifiées dans des conditions. Le programme considère une erreur si in ne les
+		// initialise pas
 		Etudiant etuBestInitiativeJ1 = null;
 		Etudiant etuBestInitiativeJ2 = null;
-		Etudiant etuBestInitiative;
+		Etudiant etuAction;
+		Etudiant etuCibleJ1 = null;
+		Etudiant etuCibleJ2 = null;
 		// On récupère l'etat de contrôle de la zone au tout debut des combats
 		Joueur statutControle = this.getEstControleePar();
 		// la condition représente la condition d'arrêt des combats (changement de
 		// statut de controle de la zone).
 		while (this.getEstControleePar() == null || statutControle == this.getEstControleePar()) {
 
+			// On crée une boucle pour récupérer en même temps l'étudiant avec le moins de
+			// credtis ECTS de cahque joueur ainsi que celui ayant la meilleure initiative.
 			Iterator<Etudiant> itJ1 = etuJoueur1.iterator();
 			while (itJ1.hasNext()) {
 				Etudiant meilleureInitiativeJ1 = itJ1.next();
 				if (meilleureInitiativeJ1.getInitiative() > bestInitiativeJ1) {
 					bestInitiativeJ1 = meilleureInitiativeJ1.getInitiative();
 					etuBestInitiativeJ1 = meilleureInitiativeJ1;
+				}
+				if (meilleureInitiativeJ1.getCreditsECTS() < moinsCreditsJ1) {
+					moinsCreditsJ1 = meilleureInitiativeJ1.getCreditsECTS();
+					etuCibleJ1 = etuBestInitiativeJ1;
 				}
 			}
 			Iterator<Etudiant> itJ2 = etuJoueur2.iterator();
@@ -53,14 +66,42 @@ public class Zone {
 					bestInitiativeJ2 = meilleureInitiativeJ2.getInitiative();
 					etuBestInitiativeJ2 = meilleureInitiativeJ2;
 				}
+				if (meilleureInitiativeJ2.getCreditsECTS() > moinsCreditsJ2) {
+					moinsCreditsJ2 = meilleureInitiativeJ2.getCreditsECTS();
+					etuCibleJ2 = meilleureInitiativeJ2;
+				}
 			}
 			// maintenant qu'on à récupéré la meilleure des initiatives des etudiants de
-			// chaque zone, on determine la meilleur entre les deux étudiants.
+			// chaque zone, on determine la meilleure entre les deux étudiants.
 			if (etuBestInitiativeJ1.getInitiative() > etuBestInitiativeJ2.getInitiative()) {
-				etuBestInitiative = etuBestInitiativeJ1;
+				etuAction = etuBestInitiativeJ1;
+			} else {
+				etuAction = etuBestInitiativeJ2;
 			}
-			else {
-				etuBestInitiative = etuBestInitiativeJ2;
+			// maintenant, on détermine l'action que notre étudiant va réaliser. Pour cela,
+			// on a besoin de récupérer la faction de l'étudaint ciblé par l'action
+
+			if (etuAction.getStrategie() == new Attaquer()) {
+				if (etuAction.getFactionEtu() == joueur1.getFactionJoueur()) {
+					etuAction.action(etuCibleJ2);
+				} else {
+					etuAction.action(etuCibleJ1);
+				}
+			} else if (etuAction.getStrategie() == new Soigner()) {
+				if (etuAction.getFactionEtu() == joueur1.getFactionJoueur()) {
+					etuAction.action(etuCibleJ1);
+				} else {
+					etuAction.action(etuCibleJ2);
+				}
+
+			} else {
+				double choixFaction = Math.random();
+				if (choixFaction < 0.5) {
+					etuAction.action(etuCibleJ1);
+				} else {
+					etuAction.action(etuCibleJ2);
+				}
+
 			}
 		}
 
@@ -156,6 +197,34 @@ public class Zone {
 	 */
 	public void setEtuJoueur2(Set<Etudiant> etuJoueur2) {
 		this.etuJoueur2 = etuJoueur2;
+	}
+
+	/**
+	 * @return the factionJ1
+	 */
+	public Faction getFactionJ1() {
+		return factionJ1;
+	}
+
+	/**
+	 * @param factionJ1 the factionJ1 to set
+	 */
+	public void setFactionJ1(Faction factionJ1) {
+		this.factionJ1 = factionJ1;
+	}
+
+	/**
+	 * @return the factionJ2
+	 */
+	public Faction getFactionJ2() {
+		return factionJ2;
+	}
+
+	/**
+	 * @param factionJ2 the factionJ2 to set
+	 */
+	public void setFactionJ2(Faction factionJ2) {
+		this.factionJ2 = factionJ2;
 	}
 
 	public static void main(String[] args) {
