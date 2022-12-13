9db946ab4c5b4516469300917eadabfeef533a5c
Vincent Delestre <vdelestre02@gmail.com>
2022-12-13 13:24:22 +0100

    une dernière petite save

--- src/fr/utt/LO02/VL/CestDuBrutal/Attaquer.java
+++ src/fr/utt/LO02/VL/CestDuBrutal/Attaquer.java
@@ -1,6 +1,7 @@
 package fr.utt.LO02.VL.CestDuBrutal;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /**
  * Classe Attaquer de notre interface Strategie
@@ -22,7 +23,7 @@ public class Attaquer implements Strategie {
 				+ etuCible.get(0).getNumEtudiant() + " faction = " + etuCible.get(0).getFactionEtu() + " pv = "
 				+ etuCible.get(0).getCreditsECTS() + " initiative = " + etuCible.get(0).getInitiative());
 		
-		int x = (int)Math.random()*101; //x est un nombre aléatoire compris entre 0 et 100
+		int x = (int)(Math.random()*101); //x est un nombre aléatoire compris entre 0 et 100
 		double y = -(Math.random()-1); //la fonction random donne un nombre aléatoire compris entre [0,1[. On soustrait 1 au nombre obtenu pour qu'il soit compris dans l'intervalle [-1,0[. On multiplie ensuite le résultat par (-1) afin d'avoir un nombre compris entre ]0,1].
 		int degatsReference = 10;
 		double coefficientDegat;
@@ -40,17 +41,21 @@ public class Attaquer implements Strategie {
 			//valMinParam correspond à la deuxième valeur en paramètre de la fonction min utilisée
 			if (valMinParam < 100) {
 				valMin = valMinParam;
+				System.out.println("valMin : "+valMin);
 			}
 			else {
 				valMin = 100;
+				System.out.println("valMin : "+valMin);
 			}
 			
 			//on crée une deuxième condition, cette fois pour déterminer le coef le plus grand entre valMin et 0 :
 			if( valMin > 0) {
 				coefficientDegat = valMin/100;
+				System.out.println("coefficientDegats : "+coefficientDegat);
 			}
 			else {
 				coefficientDegat = 0;
+				System.out.println("coefficientDegats : "+coefficientDegat);
 			}
 			
 			//Maintenant qu'on a tous nos paramètres, nous pouvons calculer les dégats infligés à l'etudiant cible.
@@ -71,7 +76,16 @@ public class Attaquer implements Strategie {
 		else {
 			System.out.println("L'attaque a échoué...");
 		}
-		
+		//Ligne de tests :
+		System.out.print("\nForce etuAction : "+etuAction.getForce()
+		+"\nResistance EtuCible : "+etuCible.get(0).getResistance());
+		System.out.println("\nx : "+x);
+		System.out.println("y : "+y);
+		System.out.println("degatsReference : "+degatsReference);
+		System.out.println("valMinParam : "+valMinParam);
+		//System.out.println("valMin : "+valMin);
+		//System.out.println("coefficientDegats : "+coefficientDegat);
+		//System.out.println("creditsECTSPerdus : "+creditsECTSPerdus);
 	}
 
 	public String nomStrat() {
@@ -79,9 +93,50 @@ public class Attaquer implements Strategie {
 	}
 
 	public static void main(String[] args) {
-		//Etudiant etu1J1 = new Etudiant("A2I",false,7,9.,1.,4.,2,1);
+		Attaquer offensive = new Attaquer();
+		//Joueur j1 = new Joueur();
+		//Joueur j2 = new Joueur();
+		//j1.setFactionJoueur(Faction.A2I);
+		//j2.setFactionJoueur(Faction.GM);
+		Etudiant etu1J1 = new Etudiant(Faction.A2I,false,7,9,1,4,2,1);
+		Etudiant etu2J1 = new Etudiant(Faction.A2I,false,2,4,8,1,5,2);
+		Etudiant etu3J1 = new Etudiant(Faction.A2I,false,4,2,5,9,6,3);
+		Etudiant etu1J2 = new Etudiant(Faction.GM,false,4,6,8,1,2,1);
+		Etudiant etu2J2 = new Etudiant(Faction.GM,false,1,3,5,4,7,2);
+		Etudiant etu3J2 = new Etudiant(Faction.GM,false,7,6,1,3,5,3);
+		ArrayList<Etudiant> etuJ1 = new ArrayList<Etudiant>();
+		ArrayList<Etudiant> etuJ2 = new ArrayList<Etudiant>();
+		etuJ1.add(etu1J1);etuJ1.add(etu2J1);etuJ1.add(etu3J1);
+		etuJ2.add(etu1J2);etuJ2.add(etu2J2);etuJ2.add(etu3J2);
+		
+		etu1J1.setStrategie(offensive);etu2J1.setStrategie(offensive);etu3J1.setStrategie(offensive);
+		etu1J2.setStrategie(offensive);etu2J2.setStrategie(offensive);etu3J2.setStrategie(offensive);
+		etu1J1.action(etuJ2, etuJ1);
+		etu2J1.action(etuJ2, etuJ1);
+		//Map<Integer,Zone> zoneTest = new LinkedHashMap<>();
 		// TODO Auto-generated method stub
-
+		
+		int x1 = (int)(Math.random()*101);
+		int x2 = (int)(Math.random()*101);
+		int x3 = (int)(Math.random()*101);
+		int x4 = (int)(Math.random()*101);
+		int x5 = (int)(Math.random()*101);
+		System.out.println(
+				"\nx1 : "+x1
+				+"\nx2 : "+x2
+				+"\nx3 : "+x3
+				+"\nx4 : "+x4
+				+"\nx5 : "+x5);
+		int x = (int)(Math.random()*101);
+		while (x != 100) {
+			x = (int)(Math.random()*101);
+		}
+		System.out.println("x : "+x);
+		double y = -(Math.random()-1);
+		while (y < 0) {
+			y = -(Math.random()-1);
+		}
+		System.out.println("y : "+y);
 	}
 
 }
--- src/fr/utt/LO02/VL/CestDuBrutal/Soigner.java
+++ src/fr/utt/LO02/VL/CestDuBrutal/Soigner.java
@@ -8,31 +8,32 @@ public class Soigner implements Strategie {
 	 * méthode Stratégie qui s'occupe du soin
 	 */
 	public void typeStrategie(ArrayList<Etudiant> etuCible,Etudiant etuAction) {
-		int x = (int)Math.random()*101; //x est un nombre aléatoire compris entre 0 et 100 
+		int x = (int)(Math.random()*101); //x est un nombre aléatoire compris entre 0 et 100 
 		double y = Math.random()*0.6;
 		
-		System.out.println("etu n°"+etuAction.getNumEtudiant() + " soigne etu n°" + etuCible.get(0).getNumEtudiant());
+		System.out.println("etu N "+etuAction.getNumEtudiant() + " soigne etu N " + etuCible.get(0).getNumEtudiant());
 		if (x >= 0 && x <= 20+6*etuAction.getDexterite()){
 			//Le soin est réussi !
-			System.out.println("Le soin est réussi !"); //Il est préférable d'informer le joueur du déroulement du soin
-			int creditsECTSGagne = (int)(y*etuCible.get(0).getConstitution());
-			
+			System.out.println("Le soin est reussi !"); //Il est préférable d'informer le joueur du déroulement du soin
+			int creditsECTSGagne = (int)(y*(10 + etuCible.get(0).getConstitution()));
+			System.out.println("y : "+y
+					+"\ncreditsECTSGagne : "+creditsECTSGagne);
 			//remplacer ça par une méthode étudiant etuCible.get(0).add() qu'il faut d'abord rajouter dans la classe Etudiant
 			etuCible.get(0).setCreditsECTS(etuCible.get(0).getCreditsECTS() + creditsECTSGagne );
 			 
 			if (etuCible.get(0).getCreditsECTS() > 30 + etuCible.get(0).getConstitution()) {
 				etuCible.get(0).setCreditsECTS(30 + etuCible.get(0).getConstitution());
 				
-				System.out.println(etuCible.get(0) + " a regagné toute sa vie !");
+				System.out.println(etuCible.get(0).getNumEtudiant() + " a regagne toute sa vie !");
 			}
 			
 			else {
-				System.out.println("etu n°"+etuCible.get(0).getNumEtudiant() + " a regagné " + creditsECTSGagne + " crédits ECTS !");
+				System.out.println("etu N "+etuCible.get(0).getNumEtudiant() + " a regagne " + creditsECTSGagne + " credits ECTS !");
 					}
 		}
 		else{
 			//Si la première condition n'est pas respectée, le soin échoue.
-			System.out.println("le soin a échoué...");
+			System.out.println("le soin echoue...");
 		}
 	}
 	public String nomStrat() {
@@ -40,16 +41,53 @@ public class Soigner implements Strategie {
 	}
 
 	public static void main(String[] args) {
-		//Commande de test :
-		double z1 = 1.2;
-		double z2 = 1.7;
-				
-		int val1 = (int)z1;
-		int val2 = (int)z2;
+		Soigner defensive = new Soigner();
+		Etudiant etu1J1 = new Etudiant(Faction.A2I,false,10,9,1,4,2,1);
+		Etudiant etu2J1 = new Etudiant(Faction.A2I,false,10,4,8,1,5,2);
+		Etudiant etu3J1 = new Etudiant(Faction.A2I,false,10,2,5,9,6,3);
+		Etudiant etu1J2 = new Etudiant(Faction.GM,false,10,6,8,1,2,1);
+		Etudiant etu2J2 = new Etudiant(Faction.GM,false,10,3,5,4,7,2);
+		Etudiant etu3J2 = new Etudiant(Faction.GM,false,10,6,1,3,5,3);
+		ArrayList<Etudiant> etuJ1 = new ArrayList<Etudiant>();
+		ArrayList<Etudiant> etuJ2 = new ArrayList<Etudiant>();
+		etuJ1.add(etu1J1);etuJ1.add(etu2J1);etuJ1.add(etu3J1);
+		etuJ2.add(etu1J2);etuJ2.add(etu2J2);etuJ2.add(etu3J2);
 		
-		System.out.println("z1 : " + z1 +"\nz2 :"+ z2 + "\nval1 :" + val1 + "\nval2 :" + val2);
+		etu1J1.setStrategie(defensive);etu2J1.setStrategie(defensive);etu3J1.setStrategie(defensive);
+		etu1J2.setStrategie(defensive);etu2J2.setStrategie(defensive);etu3J2.setStrategie(defensive);
+		etu1J1.setCreditsECTS(1);etu2J1.setCreditsECTS(1);etu3J1.setCreditsECTS(1);
+		etu1J2.setCreditsECTS(1);etu2J2.setCreditsECTS(1);etu3J2.setCreditsECTS(1);
+		etu1J1.action(etuJ2, etuJ1);
+		etu2J1.action(etuJ2, etuJ1);
+		etu3J1.action(etuJ2, etuJ1);
+		etu1J2.action(etuJ1, etuJ2);
+		etu2J2.action(etuJ1, etuJ2);
+		etu3J2.action(etuJ1, etuJ2);
 		
 		
+		/*int x1 = (int)(Math.random()*101);
+		int x2 = (int)(Math.random()*101);
+		int x3 = (int)(Math.random()*101);
+		int x4 = (int)(Math.random()*101);
+		int x5 = (int)(Math.random()*101);
+		System.out.println(
+				"\nx1 : "+x1
+				+"\nx2 : "+x2
+				+"\nx3 : "+x3
+				+"\nx4 : "+x4
+				+"\nx5 : "+x5);
+		int x = (int)(Math.random()*101);
+		while (x != 100) {
+			x = (int)(Math.random()*101);
+		}
+		System.out.println("x : "+x);
+		
+		double y = Math.random()*0.6;
+		for(int i=0;i<10;i++) {
+			y = Math.random()*0.6;
+			System.out.println("y : "+y);
+		}
+		*/
 		// TODO Auto-generated method stub
 
 	}
